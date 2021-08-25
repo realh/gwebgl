e965d36c4e285910943cbb9671fc7c3822736d6c
Tony Houghton <h@realh.co.uk>
2021-08-25 20:07:03 +0100

    More debugging

--- demo.js
+++ demo.js
@@ -4,8 +4,17 @@ import Gio from 'gi://Gio';
 import Gwebgl from 'gi://Gwebgl';
 const WebGLRenderingContext = Gwebgl.WebGLRenderingContext;
 
+let rendered = false;
+
 function render(glarea, gl) {
     print('render');
+    if (!rendered) {
+        rendered = true;
+        const context = glarea.get_context();
+        const es = context.get_use_es();
+        const version = context.get_version();
+        print(`es ${es} version ${version}`);
+    }
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
@@ -19,12 +28,31 @@ function activate(app) {
     const win = Gtk.ApplicationWindow.new(app);
     win.set_default_size(800, 600);
     const glarea = Gtk.GLArea.new();
-    //glarea.set_use_es(true);
     //glarea.set_required_version(2, 0);
+    //glarea.set_use_es(true);
     const gl = new WebGLRenderingContext();
     glarea.connect('render', () => {
         render(glarea, gl);
         return true;
+    });
+    glarea.connect('create-context', () => {
+        try {
+            let ctx;
+            if (Gtk.MAJOR_VERSION == 3) {
+                const w = glarea.get_window();
+                ctx = w.create_gl_context();
+            } else {
+                const surface = glarea.get_native().get_surface();
+                ctx = surface.create_gl_context();
+            }
+            ctx.set_debug_enabled(true);
+            ctx.set_required_version(2, 0);
+            ctx.set_use_es(true);
+            return ctx;
+        } catch (e) {
+            logError(e);
+            return null;
+        }
     });
     if (Gtk.MAJOR_VERSION == 3) {
         win.add(glarea);
--- rundemo.sh
+++ rundemo.sh
@@ -3,5 +3,6 @@ cd `dirname $0`
 builddir=`pwd`/build
 export GI_TYPELIB_PATH=${GI_TYPELIB_PATH:${GI_TYPELIB_PATH}:}${builddir}
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:${LD_LIBRARY_PATH}:}${builddir}
+export G_MESSAGES_DEBUG=all
 #gjs -m ./demo/main.js
 gjs -m ./demo.js
