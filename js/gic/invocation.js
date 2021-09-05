d314f8605c7d1f8d91aa7f9836fa28c964771646
Tony Houghton <h@realh.co.uk>
2021-09-05 22:23:23 +0100

    Fixed uniformMatrix* size calculations

--- js/gic/invocation.js
+++ js/gic/invocation.js
@@ -19,6 +19,43 @@ export function methodNeedsArrayLength(method) {
         !methodsWithNoArrayLength.includes(method);
 }
 
+// Matrix functions need the array length argument to be divided again by the
+// number of elements in the array. This returns that value, 1 if it isn't a
+// matrix method. The argument can be a string or an object as in
+// methodNeedsArrayLength().
+export function matrixSizeMultiplier(method) {
+    if (method.hasOwnProperty('name')) {
+        method = method.name;
+    }
+    if (!method.startsWith('uniformMatrix')) {
+        return 1;
+    }
+    let i = method.indexOf('From');
+    if (i == -1) {
+        i = method.length;
+    }
+    let s = method.substring(13, i - 2);
+    switch (s) {
+        case '2':
+            return 4;
+        case '3':
+            return 9;
+        case '4':
+            return 16;
+        case '2x3':
+        case '3x2':
+            return 6;
+        case '2x4':
+        case '4x2':
+            return 8;
+        case '3x4':
+        case '4x3':
+            return 12;
+        default:
+            throw new Error(`Misparsed ${s} from ${method}`);
+    }
+}
+
 // Changes a method signature from WebGL to a gl* invocation
 export function adaptMethodForInvocation(m) {
     m = copyMethod(m);
@@ -36,8 +73,13 @@ export function adaptMethodForInvocation(m) {
         m.name += 's';
         m.args = [{name: '1'}, {name: `&${m.args[0].name}`}];
     } else {
+        let arrayLength = null;
+        let matSize = 1;
         if (m.name == 'bufferData') {
             m.args.splice(2, 0, {name: 'NULL', type: 'any'});
+        } else if ((matSize = matrixSizeMultiplier(m)) > 1) {
+            arrayLength =
+                m.args.find(a => a.arrayLength != undefined)?.arrayLength;
         }
         for (let i = 0; i < m.args.length; ++i) {
             const a = m.args[i];
@@ -48,7 +90,7 @@ export function adaptMethodForInvocation(m) {
                     let div = tn.match(/([0-9]*)Array/)?.[1] || '8';
                     div = Number(div);
                     if (div !== NaN) {
-                        div /= 8;
+                        div *= matSize / 8;
                     } else {
                         div = 1;
                     }
@@ -66,7 +108,9 @@ export function adaptMethodForInvocation(m) {
                 // Mostly WebGL uses GLintptr for actual pointers, but in
                 // bufferSubData it's used for an int offset
                 a.name = '(gconstpointer) ' + a.name;
-            } 
+            } else if (matSize > 1 && arrayLength && a.name == arrayLength) {
+                a.name += ` / ${matSize}`;
+            }
         }
     }
     return m;
